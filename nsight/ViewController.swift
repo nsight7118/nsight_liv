6a3eb5960de436546f212906b2bce84da4967dc8
Nalin Pinnagoda <Nalin@nalins-mac-mini.local>
2018-08-12 17:16:45 -0400

    Initial Commit

---
+++ nsight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nsight
+//
+//  Created by Nalin Pinnagoda on 8/12/18.
+//  Copyright © 2018 Nalin Pinnagoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
